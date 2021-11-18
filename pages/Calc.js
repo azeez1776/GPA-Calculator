176669e3db98c9a20f8df61337d2b8872becb61d
azeez1776 <aboud3am81@gmail.com>
2021-11-18 16:23:10 +0300

    success in adding the react navigation and using it

--- pages/Calc.js
+++ pages/Calc.js
@@ -2,11 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import GpaInput from '../components/GpaInput';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 
-export default function Calc() {
+export default function Calc({ navigation, route }) {
 
     const [marks, setMarks] = useState({
         grade: '',
