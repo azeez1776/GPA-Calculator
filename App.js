6597cef993b1e13743aaacecb33a1f1386725b32
azeez1776 <aboud3am81@gmail.com>
2021-11-18 15:21:10 +0300

    did some optimization

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import GpaInput from './components/GpaInput';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
