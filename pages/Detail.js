9b05cc97a672761791b8c72770cbed315f10a6d9
azeez1776 <aboud3am81@gmail.com>
2021-11-24 10:42:24 +0300

    added the gpa kaluu btn, about btn and setting btn

--- pages/Detail.js
+++ pages/Detail.js
@@ -7,7 +7,13 @@ const Detail = () => {
         <View style={styles.container}>
             <View style={styles.menu}>
                 <TouchableOpacity>
-
+                    <Text>GPA Kaluu</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>About</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>Setting</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -16,4 +22,13 @@ const Detail = () => {
 
 export default Detail
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    menu: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
