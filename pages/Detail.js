fc588304adfc2e302be99ec513c28daf2bd84ada
azeez1776 <aboud3am81@gmail.com>
2021-11-24 10:39:15 +0300

    added the container view and touchable opacity

--- pages/Detail.js
+++ pages/Detail.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Detail = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <View style={styles.menu}>
+                <TouchableOpacity>
+
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
