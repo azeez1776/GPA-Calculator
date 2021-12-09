9eb2fab4a4b29b412648fe197a42444b079a4912
azeez1776 <aboud3am81@gmail.com>
2021-12-09 18:10:07 +0300

    added styling to the description text tag

--- pages/About.js
+++ pages/About.js
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.partOne}>
-                <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 10 }}>About</Text>
-                <Text>This is an app that calculates GPA based on the grades and credit entered by the user. It is a very simple to use GPA calculator</Text>
+                <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 12 }}>About</Text>
+                <Text style={{ fontSize: 23, paddingLeft: 8 }}>This is an app that calculates GPA based on the grades and credit entered by the user. It is a very simple to use GPA calculator</Text>
             </View>
 
         </SafeAreaView>
