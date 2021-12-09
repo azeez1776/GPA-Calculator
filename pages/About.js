7ddcd43d64f4b344359f5d25721249c4bb59a3e7
azeez1776 <aboud3am81@gmail.com>
2021-12-09 16:26:36 +0300

    added a screen which holds the about content

---
+++ pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const About = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default About
+
+const styles = StyleSheet.create({})
