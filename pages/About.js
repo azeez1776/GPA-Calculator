5c1dbd47a3df0cb9fc05d27678671388e4817229
azeez1776 <aboud3am81@gmail.com>
2021-12-09 18:17:32 +0300

    added some styling

--- pages/About.js
+++ pages/About.js
@@ -6,10 +6,11 @@ const About = () => {
         <SafeAreaView style={styles.container}>
             <View style={styles.partOne}>
                 <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 12 }}>About</Text>
-                <Text style={{ fontSize: 23, paddingLeft: 8 }}>This is an app that calculates GPA based on the grades and credit entered by the user. It is a very simple to use GPA calculator</Text>
+                <Text style={{ fontSize: 23, paddingLeft: 8 }}>This is an app that calculates GPA based on the grades and credit entered by the user. It is a very simple to use GPA calculator.</Text>
             </View>
             <View style={styles.partTwo}>
-
+                <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 12 }}>Grading Scale</Text>
+                <Text style={{ fontSize: 23, paddingLeft: 8 }}>A = 5</Text>
             </View>
 
         </SafeAreaView>
@@ -20,6 +21,7 @@ export default About
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        // justifyContent:'space-between'
     }
 })
