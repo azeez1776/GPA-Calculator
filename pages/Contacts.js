7cff76fc437519b8eb46cf28099d86cee15d941e
azeez1776 <aboud3am81@gmail.com>
2021-12-09 19:39:52 +0300

    initialised Contacts.js

---
+++ pages/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Contacts = () => {
+    return (
+        <View>
+            <Text>Contacts</Text>
+        </View>
+    )
+}
+
+export default Contacts
+
+const styles = StyleSheet.create({})
