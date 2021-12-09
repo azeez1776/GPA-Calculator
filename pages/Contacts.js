9a0551812746667b2ee4303371ffefac4d70c2d5
azeez1776 <aboud3am81@gmail.com>
2021-12-10 00:01:28 +0300

    added dummy info on the contacts page

--- pages/Contacts.js
+++ pages/Contacts.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 const Contacts = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Contacts</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
