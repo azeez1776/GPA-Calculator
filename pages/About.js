903ec780d73df46dddddf7671546a408bd49d309
azeez1776 <aboud3am81@gmail.com>
2021-12-09 18:08:22 +0300

    added another text tag

--- pages/About.js
+++ pages/About.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 const About = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <View>
+            <View style={styles.partOne}>
                 <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 10 }}>About</Text>
-
+                <Text>This is an app that calculates GPA based on the grades and credit entered by the user. It is a very simple to use GPA calculator</Text>
             </View>
 
         </SafeAreaView>
