00b53def3ebcdd846effbf73a51782461a1f3cbc
azeez1776 <aboud3am81@gmail.com>
2021-12-10 00:24:14 +0300

    imported icon from react native elements

--- pages/Contacts.js
+++ pages/Contacts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Linking } from 'react-native'
+import { Icon } from 'react-native-elements'
 
 const Contacts = () => {
     return (
