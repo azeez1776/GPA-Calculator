d41db2219fccf10f4b921f18dbf77412cb5e0f52
azeez1776 <aboud3am81@gmail.com>
2021-12-09 18:05:22 +0300

    added a view

--- pages/About.js
+++ pages/About.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 const About = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 10 }}>About</Text>
+            <View>
+                <Text style={{ fontSize: 27, fontWeight: 'bold', paddingLeft: 10 }}>About</Text>
+
+            </View>
+
         </SafeAreaView>
     )
 }
