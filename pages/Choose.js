e4fe711a53f8fe207da29d9fb2882edec9d583bd
azeez1776 <aboud3am81@gmail.com>
2022-02-04 23:41:46 +0300

    added another page for choice

---
+++ pages/Choose.js
@@ -0,0 +1,62 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const Detail = ({ navigation, route }) => {
+    return (
+        <View style={styles.container}>
+            <StatusBar backgroundColor='black' style='light' />
+            <View style={styles.menu}>
+                <View style={styles.menuList}>
+                    <Text style={{ fontSize: 30, fontWeight: 'bold' }}>GPA Kaluu</Text>
+                </View>
+                <TouchableOpacity
+                    style={styles.gpaKaluubtn}
+                    onPress={() => {
+                        navigation.navigate('5.0 Calculator');
+                    }}
+                >
+                    <Text style={{ color: 'white', textAlign: 'center', paddingTop: 7, fontSize: 20 }}>GPA Kaluu</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.aboutbtn}
+                    onPress={() => {
+                        navigation.navigate('4.0 Calculator')
+                    }}
+
+                >
+                    <Text style={{ color: 'white', textAlign: 'center', paddingTop: 7, fontSize: 20 }}>About</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default Detail
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    menu: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    gpaKaluubtn: {
+        backgroundColor: '#03071e',
+        width: 220,
+        height: 40,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+
+    },
+    aboutbtn: {
+        backgroundColor: '#370617',
+        width: 220,
+        height: 40,
+
+    },
+
+})
