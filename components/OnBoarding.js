74f7467dd73a537626a483e9e508dbf5fa00e79f
azeez1776 <aboud3am81@gmail.com>
2022-02-04 21:53:19 +0300

    imported the Onboarding component from react-native-onboarding-swipper

--- components/OnBoarding.js
+++ components/OnBoarding.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Onboarding from 'react-native-onboarding-swiper';
 
 const OnBoarding = () => {
     return (
         <View>
-            <Text></Text>
+
         </View>
     );
 };
